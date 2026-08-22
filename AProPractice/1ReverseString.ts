fd459bfb2c15e5c6ff0cc76e314e6b4fcebbbd61
sandiwas <harrycode9@gmail.com>
2026-08-22 22:32:18 +0530

    reverse Striung

---
+++ AProPractice/1ReverseString.ts
@@ -0,0 +1,28 @@
+class ReverseString {
+  public static reverseString(str: string): string {
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      result += str.charAt(i);
+    }
+    return result;
+  }
+
+  public static RemoveSpaceMakeLowerCase(str: string): string {
+    let lowerCase = "";
+    for (let i = 0; i < str.length; i++) {
+      let ch = str.charAt(i);
+      if (ch !== " ") {
+        if (ch >= "A" && ch <= "Z") {
+          ch = String.fromCharCode(ch.charCodeAt(0) + 32);
+        }
+        lowerCase += ch;
+      }
+    }
+    return lowerCase;
+  }
+}
+
+let str = "ja va";
+str = ReverseString.RemoveSpaceMakeLowerCase(str);
+console.log(`string without space with lowerCase : ${str}`);
+console.log(ReverseString.reverseString(str));
