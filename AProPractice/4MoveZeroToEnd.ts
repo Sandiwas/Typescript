7755ad5e1581383594125cb2422111f585fdda58
sandiwas <harrycode9@gmail.com>
2026-08-25 17:55:40 +0530

    move zero to end

---
+++ AProPractice/4MoveZeroToEnd.ts
@@ -0,0 +1,17 @@
+class MoveZeroToEnd {
+  static moveZeroToEnd(arr: number[]):number[] {
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== 0) {
+        arr[index++] = arr[i];
+      }
+    }
+    for (let i = index; i < arr.length; i++) {
+      arr[i] = 0;
+    }
+    return arr;
+  }
+}
+
+let arr = [1, 0, 3, 0, 5];
+console.log(MoveZeroToEnd.moveZeroToEnd(arr))
