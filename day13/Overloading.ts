5c62a0197d0e045827b603c8ef66ae11c8f9ae15
sandiwas <harrycode9@gmail.com>
2026-08-26 10:56:41 +0530

    method overloading

---
+++ day13/Overloading.ts
@@ -0,0 +1,34 @@
+class Calculator {
+  //Constructor overloading
+
+  constructor(); // default constructor
+  constructor(a: number, b: number); // parameterized constructor
+
+  constructor(a?: number, b?: number) {
+    if (a !== undefined && b !== undefined) {
+      console.log("sum of a and b : ", a + b);
+    } else {
+      console.log("Default constructor called");
+    }
+  }
+
+  //Method overloading
+
+  add(a: number, b: number): number;
+  add(a: number, b: number, c: number): number;
+
+  add(a: number, b: number, c?: number): number {
+    if (c !== undefined) {
+      return a + b + c;
+    }
+    return a + b;
+  }
+}
+//Usage
+
+let cal1 = new Calculator();
+let cal2 = new Calculator(2, 3);
+
+//Method overloading
+console.log("adding two number : ", cal1.add(10, 20));
+console.log("adding two number : ", cal2.add(10, 20, 30));
