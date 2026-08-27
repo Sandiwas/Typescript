e0d34b9e866cbb81632df19d6577648f68840ba1
sandiwas <harrycode9@gmail.com>
2026-08-27 13:57:22 +0530

    module

---
+++ day15/Module.ts
@@ -0,0 +1,17 @@
+export class Calculator {
+  static add(a: number, b: number): number {
+    return a + b;
+  }
+}
+
+export class ForMatter {
+  static toUppercase(str: string): string {
+    return str.toUpperCase();
+  }
+}
+
+export function multiplication(a: number, b: number): number {
+  return a * b;
+}
+
+export let appname = "Calculator";
