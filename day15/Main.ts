c5f776db1b7aeb71ffbe3953bc869fe98fc89ec0
sandiwas <harrycode9@gmail.com>
2026-08-27 13:56:56 +0530

    main

---
+++ day15/Main.ts
@@ -0,0 +1,21 @@
+// import {appname} from './Module'
+// import {Calculator} from './Module'
+// import {ForMatter} from './Module'
+// import {multiplication} from './Module'
+//import * as utils from "./Module";
+import { appname, Calculator, multiplication, ForMatter } from "./Module";
+
+// console.log(appname);
+// console.log(multiplication(2, 3));
+// console.log(Calculator.add(5, 5));
+// console.log(ForMatter.toUppercase("welcome"));
+
+// console.log(utils.appname);
+// console.log(utils.multiplication(2, 3));
+// console.log(utils.Calculator.add(5, 5));
+// console.log(utils.ForMatter.toUppercase("welcome"));
+
+console.log(appname);
+console.log(multiplication(2, 3));
+console.log(Calculator.add(5, 5));
+console.log(ForMatter.toUppercase("welcome"));
