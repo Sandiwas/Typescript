8352de2fe1ff7e49462f77b7e0f95a1cceb9654c
sandiwas <harrycode9@gmail.com>
2026-09-07 12:47:25 +0530

    all commit

--- LogicPractice/A1ReverseString.ts
+++ LogicPractice/A1ReverseString.ts
@@ -1,9 +1,10 @@
 class RevrseString {
   public static main(): void {
     let str: string = "Auto mation";
-    str=RevrseString.removeSpaceMakelowerCase(str);
-    str=RevrseString.reverseString(str);
-    console.log("Reverse String is : ",str)
+    str = RevrseString.removeSpaceMakelowerCase(str);
+    console.log("Without Space and LowerCase String is : automation : ", str);
+    str = RevrseString.reverseString(str);
+    console.log("Reverse String is : ", str);
   }
 
   public static reverseString(str: string): string {
@@ -14,10 +15,10 @@ class RevrseString {
     return result;
   }
 
-  public static removeSpaceMakelowerCase(str: string) {
-    let lowerCase = "";
+  public static removeSpaceMakelowerCase(str: string): string {
+    let lowerCase: string = "";
     for (let i = 0; i < str.length; i++) {
-      let ch = str.charAt(i);
+      let ch: string = str.charAt(i);
       if (ch !== " ") {
         if (ch >= "A" && ch <= "Z") {
           ch = String.fromCharCode(ch.charCodeAt(0) + 32);
@@ -29,5 +30,20 @@ class RevrseString {
   }
 }
 
-
 RevrseString.main();
+
+/* 
+Java → TypeScript main changes
+| Java                        | TypeScript                                   |
+| --------------------------- | -------------------------------------------- |
+| `String`                    | `string`                                     |
+| `char`                      | `string`                                     |
+| `charAt(i)`                 | `charAt(i)`                                  |
+| `(char)(ch + 32)`           | `String.fromCharCode(ch.charCodeAt(0) + 32)` |
+| `System.out.println()`      | `console.log()`                              |
+| `public static void main()` | `public static main(): void`                 |
+ */
+
+// Interviewer agar pooche: "Why charCodeAt()?"
+// Tum bol sakte ho:
+// "I am manually converting uppercase characters to lowercase using their ASCII/Unicode character codes. The difference between uppercase and lowercase English letters is 32."
