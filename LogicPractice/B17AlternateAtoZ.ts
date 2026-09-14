27d112aacaca4e6dc985af14db8cc283fceabb1e
sandiwas <harrycode9@gmail.com>
2026-09-14 10:43:13 +0530

    Alternet A 2 Z

--- LogicPractice/B17AlternateAtoZ.ts
+++ LogicPractice/B17AlternateAtoZ.ts
@@ -1,10 +1,18 @@
 class AlternateAtoZ {
   public static alternet() {
-    for (let ch = "A"; ch <= "Z"; ch += 2) {
-      console.log(`$ch`);
+    let result: string = "";
+    for (let ch = "A".charCodeAt(0); ch <= "Z".charCodeAt(0); ch ++) {
+      result = result + String.fromCharCode(ch) + " ";
     }
+    console.log(result);
   }
 }
 
-
 AlternateAtoZ.alternet();
+
+/* 
+| Method                    | Conversion             |
+| ------------------------- | ---------------------- |
+| `'A'.charCodeAt(0)`       | **Character → Number** |
+| `String.fromCharCode(65)` | **Number → Character** |
+ */
