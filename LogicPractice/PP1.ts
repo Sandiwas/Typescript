b257e2d300fb25f95fe6ddb893687bc40bbbf046
sandiwas <harrycode9@gmail.com>
2026-09-25 20:18:08 +0530

    .\PP1.ts

---
+++ LogicPractice/PP1.ts
@@ -0,0 +1,12 @@
+class PP11 {
+  public static main() {
+    for (let i = 1; i <= 4; i++) {
+      let list = "";
+      for (let j = 1; j <= 4; j++) {
+        list += "*";
+      }
+      console.log(list);
+    }
+  }
+}
+PP11.main();
